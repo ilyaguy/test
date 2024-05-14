a4d07cf41d720306d7e0517f82b6896ed1d7a139
Illia Hai <ilya.daemon@gmail.com>
2024-05-14 22:57:37 +0300

    Revert brocken test

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,8 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Requestl;
 
-Route::get('/', [WeatherController::class, 'index']);
+Route::get('/weather', [WeatherController::class, 'index']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
